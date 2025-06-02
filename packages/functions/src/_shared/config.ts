fcbef4281ad83f09e1ea39b13dfa961ef786edb0
dbawebdesign <dbawebdesign1@gmail.com>
2025-06-02 11:13:22 -0400

    Update LOGIN_REDIRECT_URL to support multiple environment variables for improved flexibility
    - Modified the LOGIN_REDIRECT_URL to prioritize the NEXT_PUBLIC_APP_URL and VERCEL_URL environment variables, providing a more dynamic and adaptable redirect URL configuration.
    - Ensured fallback to 'http://localhost:3000/' if no environment variables are set, maintaining local development compatibility.

--- packages/functions/src/_shared/config.ts
+++ packages/functions/src/_shared/config.ts
@@ -1,1 +1,3 @@
-export const LOGIN_REDIRECT_URL = Deno.env.get('LOGIN_REDIRECT_URL') || 'http://localhost:3000/'; 
+export const LOGIN_REDIRECT_URL = Deno.env.get('LOGIN_REDIRECT_URL') || 
+  Deno.env.get('NEXT_PUBLIC_APP_URL') || 
+  (Deno.env.get('VERCEL_URL') ? `https://${Deno.env.get('VERCEL_URL')}` : 'http://localhost:3000/'); 
